be63908e395bb083116938cb89f7ed203585be9e
Daniel Gahler <darkshoxx@yahoo.de>
2024-11-12 14:14:22 +0000

    runidemtary prototype

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FMVEngine
 Tkinter based FMV Window Game Engine
+
+Plan:
+- 3 Modes:
+    - Play
+    - Edit
+    - Test (A button that tests all combinations and unlocks "Play")
+
+- Edit mode:
+    - Create Scene
+    - Define Clickable Region
+    - Define Action (click, verb, end of cutscene)
+        - action leads to change in scene, inventory
---
+++ main.py
@@ -0,0 +1,144 @@
+from tkinter import END, Listbox, Scrollbar, Tk, Frame, Label, Button, W,S,E,N,SW,SE, BOTH
+from PIL import Image
+from PIL.ImageTk import PhotoImage
+
+
+# files
+import os
+from typing import List
+HERE = os.path.abspath(os.path.dirname(__file__))
+IMAGE_FOLDER = os.path.join(HERE, "Images")
+DEFAULT_IMAGE = os.path.join(IMAGE_FOLDER, "default.png")
+IMAGE_WIDTH = 800
+IMAGE_HEIGHT = 600
+
+def load_images(directory:str = IMAGE_FOLDER)->List[PhotoImage]:
+    image_list = []
+    for (dir_path, dir_names, file_names) in os.walk(directory):
+        for file in file_names:
+            current_image = Image.open(os.path.join(dir_path, file))
+            current_image = current_image.resize((IMAGE_WIDTH, IMAGE_HEIGHT))
+            image_list.append(current_image)
+    return image_list
+
+def draw_scene_list(parent:Frame)-> Frame:
+    default_scenes = ["Boxing", "Hallway", "HallwayWest", "Office"]
+    scenes_frame = Frame(parent, name="scene_frame", height=100, width=120)
+    scene_box = Listbox(scenes_frame, height=4, width=60)
+    for item in default_scenes:
+        scene_box.insert(END, item)
+    scene_box.grid(row=0, column=0, sticky=E, pady=2)
+    scene_scrollbar = Scrollbar(scenes_frame)
+    scene_scrollbar.grid(row=0, column=1, sticky=W, pady=2)
+    scene_box.config(yscrollcommand =scene_scrollbar.set)
+    scene_scrollbar.config(command=scene_box.yview)
+    return scenes_frame
+
+
+def draw_main_window(edit_mode:bool = True):
+    # init values
+    scene_index = 0
+    scene_title = "untitled"
+
+    root_edit = Tk()
+    root_edit.resizable(False, False)
+    IMAGES_LIST = [PhotoImage(file=DEFAULT_IMAGE)] 
+    play_frame = Frame(root_edit, height=IMAGE_WIDTH, width=IMAGE_HEIGHT+100)
+    if edit_mode:
+        root_edit.title("Edit Window")
+        root_edit.geometry("1200x900")
+        play_frame.grid(row=1, column=1, sticky=SE, pady=2, rowspan=2, columnspan=2) 
+    else:
+        root_edit.title("Play Window")
+        root_edit.geometry(f"{IMAGE_WIDTH}x{IMAGE_HEIGHT+100}")
+        play_frame.grid(row=0, column=0) 
+
+
+    play_window = draw_play_window(parent=play_frame, image_list=IMAGES_LIST)
+
+    play_window.grid(row=0, column=0, sticky=SE, pady=2)
+
+
+    if edit_mode:
+        details_frame = Frame(root_edit, height=200)
+        label_scene_index = Label(details_frame, text=f"current index:{scene_index}")
+        label_scene_title = Label(details_frame, text=f"current title:{scene_title}")
+        label_scene_index.grid(row=0, column=0, sticky=W, pady=2) 
+        label_scene_title.grid(row=0, column=1, sticky=W, pady=2) 
+        scene_listbox = draw_scene_list(details_frame)
+        scene_listbox.grid(row=0, column=2, sticky=W, pady=2) 
+        details_frame.grid(row=0, column=1, columnspan=2)
+
+        buttons_frame = Frame(root_edit, width=400)
+        button_define_click_region = Button(buttons_frame, text="define click region")
+        button_define_action = Button(buttons_frame, text="define action")
+        button_define_combination = Button(buttons_frame, text="define combination")
+        button_define_click_region.grid(row=0, column=0, sticky=W, pady=2) 
+        button_define_action.grid(row=1, column=0, sticky=W, pady=2) 
+        button_define_combination.grid(row=2, column=0, sticky=W, pady=2)
+        buttons_frame.grid(row=1, column=0, rowspan=2)
+
+        beeg_buttons_frame = Frame(root_edit, width=400, height=200)
+        button_define_test = Button(beeg_buttons_frame, text="TEST", height=13, width = 25, bg="red")
+        button_define_play = Button(beeg_buttons_frame, text="PLAY", height=13, width = 25, state="disabled")
+        button_define_test.grid(row=0, column=0)
+        button_define_play.grid(row=0, column=1)
+
+        beeg_buttons_frame.grid(row=0, column=0)
+
+    root_edit.mainloop()
+    
+
+
+
+def draw_play_window(parent: Frame = None, image_list: List = None) -> Frame:
+    # relabel for convenience
+    root_play = parent
+
+    # define frame for dimensions
+    play_frame = Frame(root_play, width=800, height=700)
+    play_frame.grid(row=0, column=0)
+    # get current image  TODO
+    current_image = image_list[0]
+    image_label = Label(play_frame, image=current_image)
+
+    # define buttons
+    button_labels = ["Use", "Pick Up", "Go", "Look", "Push", "Pull"]
+    button_width=17
+    buttons_frame = Frame(play_frame, name="buttons_frame", width=400, height=100)
+    button_list = []
+    for index in range(6):
+        button_list.append(Button(buttons_frame,width=button_width, text=button_labels[index]))
+    button_pad_y = 5
+    button_pad_x = 2
+    button_list[0].grid(row=0, column=0, pady=button_pad_y, padx=button_pad_x)
+    button_list[1].grid(row=0, column=1, pady=button_pad_y, padx=button_pad_x)
+    button_list[2].grid(row=0, column=2, pady=button_pad_y, padx=button_pad_x)
+    button_list[3].grid(row=1, column=0, pady=button_pad_y, padx=button_pad_x)
+    button_list[4].grid(row=1, column=1, pady=button_pad_y, padx=button_pad_x)
+    button_list[5].grid(row=1, column=2, pady=button_pad_y, padx=button_pad_x)
+    
+    # define inventory box
+    default_inventory_items = ["Rope", "Bottle", "Newspaper", "Book", "Coins"]
+    inventory_frame = Frame(play_frame, name="inventory_frame", height=100, width=120)
+    inventory_box = Listbox(inventory_frame, height=4, width=60)
+    for item in default_inventory_items:
+        inventory_box.insert(END, item)
+    inventory_box.grid(row=0, column=0, sticky=E, pady=2)
+    scrollbar = Scrollbar(inventory_frame)
+    scrollbar.grid(row=0, column=1, sticky=W, pady=2)
+    inventory_box.config(yscrollcommand =scrollbar.set)
+    scrollbar.config(command=inventory_box.yview)
+
+    # place in frame
+    image_label.place(x=0,y=0)
+    buttons_frame.place(x=5,y=612)
+    inventory_frame.place(x=410,y=612)
+
+    return play_frame
+
+
+if __name__ == "__main__":
+    load_images()
+    draw_main_window(edit_mode=True)
+
